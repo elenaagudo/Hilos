2a7c17adf856b4928e8aef5f6d4b239b0941cb91
elenaagudo <elenaagudo17@gmail.com>
2020-10-31 17:47:21 +0100

    Segundo commit: Proyecto terminado

--- src/hilos/Consumidor.java
+++ src/hilos/Consumidor.java
@@ -1,5 +1,22 @@
 package hilos;
 
 public class Consumidor extends Thread {
+	private Tuberia tuberia;
+
+	public Consumidor(Tuberia tuberia) {
+		this.tuberia = tuberia;
+	}
+
+	public void run() {
+		char c;
+		for (int i = 0; i < 10; i++) {
+			c = tuberia.recoger();
+			System.out.println("Recogido el caracter " + c);
+			try {
+				sleep((int) (Math.random() * 2000));
+			} catch (InterruptedException e) {
+			}
+		}
+	}
 
 }
--- src/hilos/Launcher.java
+++ src/hilos/Launcher.java
@@ -3,7 +3,12 @@ package hilos;
 public class Launcher {
 
 	public static void main(String[] args) {
+		Tuberia t = new Tuberia();
+		Productor p = new Productor(t);
+		Consumidor c = new Consumidor(t);
 		
+		p.start();
+		c.start();
 	}
 
 }
